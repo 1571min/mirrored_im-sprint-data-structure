bb3e2a822144c10b3138b33b25a8786a1be0a1e5
lmt <1571min@naver.com>
2020-03-20 12:15:33 +0900

    completed pseudoclassical

--- part-one/src/pseudoclassical/queue.js
+++ part-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 const Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.front = 0;
+  this.rear = 0;
+};
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.rear++] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.rear !== this.front) {
+    this.count--;
+    return this.storage[this.front++];
+  }
+  return undefined;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
 };
 
 module.exports = {
