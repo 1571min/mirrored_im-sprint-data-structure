128d7bde2b1c3fd429054b9f997b8dcc6695d5cf
lmt <1571min@naver.com>
2020-03-20 12:09:20 +0900

    completed functional-shared

--- part-one/src/functional-shared/queue.js
+++ part-one/src/functional-shared/queue.js
@@ -1,11 +1,41 @@
-const Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 const queueMethods = {};
+queueMethods.enqueue = function(value) {
+  this.storage[this.rear++] = value;
+  this.qsize++;
+};
 
-if (typeof module === "object" && typeof module.exports === "object") {
+queueMethods.dequeue = function() {
+  if (this.rear !== this.front) {
+    this.qsize--;
+    return this.storage[this.front++];
+  }
+  return undefined;
+};
+
+queueMethods.size = function() {
+  return this.qsize;
+};
+
+const Queue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var queueInstance = {
+    storage: {},
+    front: 0,
+    rear: 0,
+    qsize: 0
+  };
+  extend(queueInstance, queueMethods);
+  return queueInstance;
+};
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Queue,
     queueMethods
--- part-one/src/functional-shared/stack.js
+++ part-one/src/functional-shared/stack.js
@@ -1,11 +1,38 @@
-const Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 const stackMethods = {};
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
-if (typeof module === "object" && typeof module.exports === "object") {
+stackMethods.pop = function() {
+  if (this.count !== 0) {
+    return this.storage[this.count-- - 1];
+  }
+  return undefined;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
+
+const Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var stackInstance = {
+    storage: {},
+    count: 0
+  };
+  extend(stackInstance, stackMethods);
+  return stackInstance;
+};
+
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Stack,
     stackMethods
