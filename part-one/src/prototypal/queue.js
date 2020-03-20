8958379b474937e46a494b1caf72247737c7ae27
lmt <1571min@naver.com>
2020-03-20 12:10:06 +0900

    completed prototypal

--- part-one/src/prototypal/queue.js
+++ part-one/src/prototypal/queue.js
@@ -1,9 +1,32 @@
 const Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = Object.create(queueMethods);
+  queueInstance.storage = {};
+  queueInstance.count = 0;
+  queueInstance.front = 0;
+  queueInstance.rear = 0;
+  return queueInstance;
 };
 
 const queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.rear++] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.rear !== this.front) {
+    this.count--;
+    return this.storage[this.front++];
+  }
+  return undefined;
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
 
 module.exports = {
   Queue,
--- part-one/src/prototypal/stack.js
+++ part-one/src/prototypal/stack.js
@@ -1,11 +1,42 @@
 const Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.storage = {};
+  stackInstance.count = 0;
+  return stackInstance;
 };
 
 const stackMethods = {};
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function() {
+  if (this.count !== 0) {
+    return this.storage[this.count-- - 1];
+  }
+  return undefined;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
 
 module.exports = {
   Stack,
   stackMethods
 };
+
+// var someMethods = {};
+// someMethods.move = function () {
+//   this.position += 1;
+// };
+
+// var Car = function (position) {
+//   var someInstance = Object.create(someMethods);
+//   someInstance.position = position;
+//   return someInstance;
+// }
+// var car1 = Car(5);
