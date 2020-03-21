c3fe68a713fe1bf34e395d4effa97305732098f4
lmt <1571min@naver.com>
2020-03-21 23:06:25 +0900

    completed hashTable advanced

--- part-two/__test__/hashTable.test.js
+++ part-two/__test__/hashTable.test.js
@@ -1,21 +1,21 @@
-const HashTable = require("../src/hashTable");
-const _ = require("underscore");
+const HashTable = require('../src/hashTable');
+const _ = require('underscore');
 
 let {
   LimitedArray,
   getIndexBelowMaxForKey
-} = require("../src/hashTableHelpers");
+} = require('../src/hashTableHelpers');
 
-describe("hashTable", function() {
+describe('hashTable', function() {
   let hashTable;
   let people = [
-    ["Steven", "Tyler"],
-    ["George", "Harrison"],
-    ["Mr.", "Doob"],
-    ["Dr.", "Sunshine"],
-    ["John", "Resig"],
-    ["Brendan", "Eich"],
-    ["Alan", "Turing"]
+    ['Steven', 'Tyler'],
+    ['George', 'Harrison'],
+    ['Mr.', 'Doob'],
+    ['Dr.', 'Sunshine'],
+    ['John', 'Resig'],
+    ['Brendan', 'Eich'],
+    ['Alan', 'Turing']
   ];
 
   beforeEach(function() {
@@ -23,36 +23,36 @@ describe("hashTable", function() {
   });
 
   it('should have methods named "insert", "remove", and "retrieve', function() {
-    expect(hashTable).toHaveProperty("insert");
-    expect(hashTable).toHaveProperty("remove");
-    expect(hashTable).toHaveProperty("retrieve");
+    expect(hashTable).toHaveProperty('insert');
+    expect(hashTable).toHaveProperty('remove');
+    expect(hashTable).toHaveProperty('retrieve');
   });
 
-  it("should store values that were inserted", function() {
-    hashTable.insert("Steven", "Seagal");
-    expect(hashTable.retrieve("Steven")).toEqual("Seagal");
+  it('should store values that were inserted', function() {
+    hashTable.insert('Steven', 'Seagal');
+    expect(hashTable.retrieve('Steven')).toEqual('Seagal');
   });
 
-  it("should not contain values that were not inserted", function() {
-    hashTable.insert("Steven", "Spielberg");
-    expect(hashTable.retrieve("Steven")).not.toEqual("Seagal");
+  it('should not contain values that were not inserted', function() {
+    hashTable.insert('Steven', 'Spielberg');
+    expect(hashTable.retrieve('Steven')).not.toEqual('Seagal');
   });
 
-  it("should overwrite values that have the same key", function() {
-    hashTable.insert("Bob", "Loblaw");
-    hashTable.insert("Bob", "Barker");
-    expect(hashTable.retrieve("Bob")).toEqual("Barker");
+  it('should overwrite values that have the same key', function() {
+    hashTable.insert('Bob', 'Loblaw');
+    hashTable.insert('Bob', 'Barker');
+    expect(hashTable.retrieve('Bob')).toEqual('Barker');
   });
 
-  it("should not contain values that were removed", function() {
-    hashTable.insert("Steven", "Tyler");
-    hashTable.remove("Steven");
-    expect(hashTable.retrieve("Steven")).toEqual(undefined);
+  it('should not contain values that were removed', function() {
+    hashTable.insert('Steven', 'Tyler');
+    hashTable.remove('Steven');
+    expect(hashTable.retrieve('Steven')).toEqual(undefined);
   });
 
-  it("should handle hash function collisions", function() {
-    var v1 = "val1";
-    var v2 = "val2";
+  it('should handle hash function collisions', function() {
+    var v1 = 'val1';
+    var v2 = 'val2';
     var oldHashFunction = getIndexBelowMaxForKey;
     getIndexBelowMaxForKey = function() {
       return 0;
@@ -65,7 +65,7 @@ describe("hashTable", function() {
   });
 
   // (Advanced! Remove the extra "x" when you want the following tests to run)
-  xit("should double in size when needed", function() {
+  it('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
@@ -75,7 +75,7 @@ describe("hashTable", function() {
     expect(hashTable._limit).toEqual(16);
   });
 
-  xit("should halve in size when needed", function() {
+  it('should halve in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
@@ -83,11 +83,11 @@ describe("hashTable", function() {
       expect(hashTable.retrieve(firstName)).toEqual(lastName);
     });
     expect(hashTable._limit).toEqual(16);
-    hashTable.remove("George");
-    hashTable.remove("Dr.");
-    hashTable.remove("Steven");
-    hashTable.remove("John");
-    hashTable.remove("Mr.");
+    hashTable.remove('George');
+    hashTable.remove('Dr.');
+    hashTable.remove('Steven');
+    hashTable.remove('John');
+    hashTable.remove('Mr.');
     expect(hashTable._limit).toEqual(8);
   });
 });
--- part-two/src/hashTable.js
+++ part-two/src/hashTable.js
@@ -1,20 +1,99 @@
-const { LimitedArray, getIndexBelowMaxForKey } = require("./hashTableHelpers");
+const { LimitedArray, getIndexBelowMaxForKey } = require('./hashTableHelpers');
 
 const HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
+// 해쉬 테이블 삽입 함수
+// 1. 해당 인덱스에 값이 있는 지 없는 지 확인
+// 2. 없다면 배열과 배열에 값을 추가해 대입
+// 3. 있다면 해당 배열을 순회하면서 undefined에 있는 값에 대입
+//const invalidNameAndHeight: [string, number] = ['안희종', 176, 42];
 HashTable.prototype.insert = function(k, v) {
+  // 카운팅해서 크기 증가할지 결정
+  var count = 0;
+  this._storage.each(function(ele) {
+    if (ele !== undefined) {
+      ele.forEach(() => {
+        count++;
+      });
+    }
+  });
+  if (((count + 1) / this._limit) * 100 > 75) {
+    this._limit *= 2;
+    let tempStorage = LimitedArray(this._limit);
+    this._storage.each(function(ele, idx) {
+      tempStorage[idx] = ele;
+    });
+    this._storage = tempStorage;
+  }
+
+  // 튜플형식의 tempEle선언
   const index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) {
+    let tempArr = this._storage.get(index);
+    let tempEle = [k, v];
+    tempArr.push(tempEle);
+    this._storage.set(index, tempArr);
+  } else {
+    let tempArr = [];
+    let tempEle = [k, v];
+    tempArr.push(tempEle);
+    this._storage.set(index, tempArr);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   const index = getIndexBelowMaxForKey(k, this._limit);
+  let tempArr = this._storage.get(index);
+  let result = undefined;
+  if (tempArr !== undefined) {
+    tempArr.forEach(function(ele) {
+      if (k === ele[0]) {
+        result = ele[1];
+      }
+    });
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   const index = getIndexBelowMaxForKey(k, this._limit);
+
+  //filter를 이용해서 걸러내고 다시 저장
+  let tempArr = this._storage.get(index);
+  if (tempArr !== undefined) {
+    tempArr = tempArr.filter(function(ele) {
+      if (k === ele[0]) {
+        return false;
+      }
+      return true;
+    });
+    if (tempArr.length === 0) {
+      this._storage.set(index, undefined);
+    } else {
+      this._storage.set(index, tempArr);
+    }
+  }
+
+  // 카운팅해서 resize 구현
+  var count = 0;
+  this._storage.each(function(ele) {
+    if (ele !== undefined) {
+      ele.forEach(() => {
+        count++;
+      });
+    }
+  });
+  if ((count / this._limit) * 100 < 25 && this._limit > 8) {
+    this._limit /= 2;
+    let tempStorage = LimitedArray(this._limit);
+    this._storage.each(function(ele, idx) {
+      tempStorage[idx] = ele;
+    });
+    this._storage = tempStorage;
+  }
 };
 
 /*
