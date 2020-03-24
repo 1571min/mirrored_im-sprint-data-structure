2d761b3e5e87872542e7943783a29dcc18486243
lmt <1571min@naver.com>
2020-03-24 12:07:29 +0900

    completed binarySearchTree.js

--- part-two/src/binarySearchTree.js
+++ part-two/src/binarySearchTree.js
@@ -1,7 +1,62 @@
-const BinarySearchTree = function(value) {};
+const BinarySearchTree = function(value) {
+  const newTree = {};
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  extend(newTree, treeMethods);
+  return newTree;
+};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+const treeMethods = {};
+treeMethods.insert = function(value) {
+  if (this.value === undefined) {
+    this.value = value;
+  } else {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(value);
+      } else {
+        this.insert.bind(this.left)(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(value);
+      } else {
+        this.insert.bind(this.right)(value);
+      }
+    }
+  }
+};
 
+treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    if (this.value > target) {
+      if (this.left !== null) return this.left.contains(target);
+    } else {
+      if (this.right !== null) return this.right.contains(target);
+    }
+  }
+  return false;
+};
+
+treeMethods.depthFirstLog = function(value) {
+  value(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(value);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(value);
+  }
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+module.exports = BinarySearchTree;
 
 module.exports = BinarySearchTree;
